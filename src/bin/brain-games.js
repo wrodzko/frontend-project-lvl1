bbfe15f71dca22afdb4980dd272633ceccce4f02
Uladzislau Radzko <uladzislau.radzko@gmail.com>
2019-09-29 19:06:36 +0300

    Add interactive logic with user

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
+import getUserName from '.';
+
+console.log('---------------------------');
 console.log('Welcome to the Brain Games!');
+console.log('---------------------------');
+
+getUserName();
---
+++ src/bin/index.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+  console.log('May I have your name?');
+
+  const user = readlineSync.question('Type your name... ');
+  
+  console.log('---------------------------');
+  console.log(`Hello, ${user}!`);
+}
