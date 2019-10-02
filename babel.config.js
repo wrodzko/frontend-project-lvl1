5196082c6e07b0d37a6759b1e02015dc40a8c370
Uladzislau Radzko <uladzislau.radzko@gmail.com>
2019-10-02 13:48:52 +0300

    Remove browsers settings from babel config

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,7 @@ module.exports = {
     ['@babel/env', {
       targets: {
         node: 'current',
-        firefox: '60',
-        chrome: '67',
-        safari: '11.1'
-      }
-    }]
-  ]
-}
+      },
+    }],
+  ],
+};
